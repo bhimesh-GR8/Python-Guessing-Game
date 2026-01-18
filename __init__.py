93d772946e9279ff6307ee2ea815246f6019b7ee
bhimesh-GR8 <bhimesh5356@xavier.edu.np>
2026-01-18 10:07:20 +0545

    Fix author name formatting in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 """Number Guessing Game - A fun interactive guessing game."""
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "  Bhimesh"
 __description__ = "An interactive number guessing game where players try to guess a random number between 1 and 100."
 
 from .game import play_game
 
 __all__ = ["play_game"]
+
